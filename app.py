d36120d50954e982ad32fed0b87687e5049e1b7d
MaxPrimoMershon <mershon.max16@gmail.com>
2020-10-25 20:26:02 -0700

    cleaned up ui

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# cd "C:\Users\Owner\AppData\Local\Programs\Python\Python38-32\fortune500app"
+
 from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_bootstrap import Bootstrap
 import pandas as pd
@@ -10,10 +12,14 @@ app.secret_key = 'development key'
 data = pd.read_excel("Fortune500_2020_Final.xlsx")
 data.index.name = None
 
+reports = str((1 - data['Diversity Report Title'].value_counts(normalize=True)['N'])*100) + '%'
+leaders = str((1 - data['Diversity Leader'].value_counts(normalize=True)['N'])*100) + '%'
+pages = str((1 - data['Landing Page'].value_counts(normalize=True)['N'])*100) + '%'
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=data, data_table=data.to_html(classes='table')) 
+    return render_template('index.html', data=data, data_table=data.to_html(classes='table'),
+                            reports=reports, leaders=leaders, pages=pages) 
 
 
 if __name__ == "__main__":
